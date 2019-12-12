bce9a5cf4d6b4f031551675975b355fced4f1c80
SravanKumarReddy1990 <bskreddy1990@gmail.com>
2019-12-12 08:37:00 +0530

    Second

--- gtx/webkit/webkitjsex.c
+++ gtx/webkit/webkitjsex.c
@@ -126,7 +126,7 @@ main (int argc, char* argv[])
     g_signal_connect (G_OBJECT (main_window), "destroy", G_CALLBACK (destroy), NULL);
     
     /* Open webpage */
-    webkit_web_view_load_uri (WEBKIT_WEB_VIEW (web_view), "file:///home/ramu/quirc-master/gtx/webkit-class.html");
+    webkit_web_view_load_uri (WEBKIT_WEB_VIEW (web_view), "file:///home/ramu/quirc-master/gtx/webkit/index.html");
 
     /* Create the main window */
     gtk_window_set_default_size (GTK_WINDOW (main_window), 800, 600);
